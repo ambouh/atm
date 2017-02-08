7d96f201318f67cb35f7b1cb2480e9a1467001f8
ambouh <aphricanboi@gmail.com>
2017-02-08 10:31:49 -0500

    deposit can get the current bal (5)

--- atm.js
+++ atm.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-    var amount = 0;
+    var amount;
     $('#checking').on('click', function(e) {
          var selected = $(e.target),
              deposit = $('.deposit'),
-             input;
+             input,
+             checkingBal = $('#checking .balance');
+            amount = parseInt(checkingBal.html().match(/\d+$/)[0]);
 
          if (selected.is(deposit)){
              input = $('input').val();
              amount += parseInt(input);
              console.log(amount);
-             $('#checking .balance').html("$ " + amount);
+             checkingBal.html("$ " + amount);
          }
 
 
