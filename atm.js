ac43208e04b4d2bcf16346880d68a38bf221a66f
ambouh <aphricanboi@gmail.com>
2017-02-08 10:19:15 -0500

    updates deposit balance

--- atm.js
+++ atm.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-     var deposit = $('.deposit').on('click', function() {
-        console.log("Hello World");
+    var amount = 0;
+    $('#checking').on('click', function(e) {
+         var selected = $(e.target),
+             deposit = $('.deposit'),
+             input;
+
+         if (selected.is(deposit)){
+             input = $('input').val();
+             amount += parseInt(input);
+             console.log(amount);
+             $('#checking .balance').html("$ " + amount);
+         }
+
+
+
+
      });
 });
