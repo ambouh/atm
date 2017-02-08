add892432ee153e265875d034f65df55a1744b8c
ambouh <aphricanboi@gmail.com>
2017-02-08 10:48:45 -0500

    withdraw can subtract balance (7)

--- atm.js
+++ atm.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-    var amount;
+    var amount,
+        checkingBal = $('#checking .balance');
+
     $('#checking').on('click', function(e) {
          var selected = $(e.target),
              deposit = $('.deposit'),
-             input,
-             checkingBal = $('#checking .balance');
-            amount = parseInt(checkingBal.html().match(/\d+$/)[0]);
+             withdraw = $('.withdraw'),
+             input;
 
+
+        amount = parseInt(checkingBal.html().match(/\d+$/)[0]);
+        input = parseInt($('input').val());
          if (selected.is(deposit)){
-             input = $('input').val();
-             amount += parseInt(input);
-             console.log(amount);
+             amount += input;
              checkingBal.html("$ " + amount);
+         }
+
+         if (selected.is(withdraw) && amount >= input) {
+             amount-=input;
+             checkingBal.html("$ " + amount);
+         } else if(selected.is(withdraw)) {
+             checkingBal.addClass("zero");
          }
 
 
